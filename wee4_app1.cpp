5cb77452e0c92052efe3926a1befaab68daa6cd9
muhsinertugruldogan <ertugrul.dogan@ozu.edu.tr>
2023-10-28 23:30:30 +0300

    print function ass free function and const keyword

--- wee4_app1.cpp
+++ wee4_app1.cpp
@@ -1,13 +1,9 @@
-/**
- *
- *
- */
-
-
-
 #include <iostream>
 
 struct IntVector {
+    /**
+     * you can write here const to make size read-only.
+     */
     int size;
     int* data;
 
@@ -25,7 +21,14 @@ struct IntVector {
         this->data[index] = value;
     }
 
-    void print() {
+    /**
+     * this print method inside we can change size and data attributes. const is important.
+     */
+    void print() const
+    {
+        /**
+         * const and reference is very important!
+         */
         for(int i = 0; i < this->size; i++) {
             std::cout << this->data[i];
             if(i != this->size-1)
@@ -33,9 +36,22 @@ struct IntVector {
         }
         std::cout << std::endl;
     }
-};
-// lets write print function. it will be free function because printing is not only for IntVector object.
 
+
+};
+
+// let's write print function. it will be free function because printing is not only for IntVector object.
+void print(const IntVector& iv) {
+    /**
+     * const and reference is very important!
+     */
+    for(int i = 0; i < iv.size; i++) {
+        std::cout << iv.data[i];
+        if(i != iv.size-1)
+            std::cout << ", ";
+    }
+    std::cout << std::endl;
+}
 
 int main(int argc, char* argv[]) {
     {
@@ -45,8 +61,8 @@ int main(int argc, char* argv[]) {
         iv.setIndex(0,10);
         iv.setIndex(1,25);
         iv.setIndex(2,5);
-
-        iv.print();
+//        iv.print();
+        print(iv);
     }
 
     return 0;
