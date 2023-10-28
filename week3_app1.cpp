0f1b552e1646d01eab2e37b696b74275dfe25456
muhsinertugruldogan <ertugrul.dogan@ozu.edu.tr>
2023-10-28 19:45:02 +0300

    operator overloading.

--- week3_app1.cpp
+++ week3_app1.cpp
@@ -33,33 +33,19 @@
  */
 #include <iostream>
 
-int i = 10; // global scope, it is static initialization. it is not in stack, not in heap.
+int i = 10;
 
 struct Vector2d {
-    int a;  // member variable. attribute is used in OOP.
+    int a;
     int b;
-    char* c;
-    char* d;
 
-
-    Vector2d(int a, int b) {
-        c = new char[100];
-        d = new char[100];
-        this -> a = a;
-        this -> b = b;
+    Vector2d() : a(0), b(0) { // it is more speedy version of constructor.
+    }
+    Vector2d(int a_, int b_) : a(a_), b(b_){
     }
 
-    ~Vector2d() {
-        // destructor.
-        // deleting a variable and array is different.
-        // new kelimesini kullandıklarını sil. diğerlerine gerek yok.
-        // RAII principle. If you use it cleverly, garbage collector becomes insufficient.
 
-        delete d;
-        delete[] c;
-    }
-
-    Vector2d addVectors(const Vector2d& second) { // const keyword
+    Vector2d addVectors(const Vector2d& second) { // why we remember function name? we can overload operator.
         Vector2d result;
 
         result.a = this-> a + second.a;
@@ -68,32 +54,28 @@ struct Vector2d {
         // copy it to object inside.
     }
 
-//    int a;  attributelar class ın başında olmak zorunda değil. compiler ilk önce attribute ları görür.
-//    int b;
-//    char* c;
-//    char* d;
+    Vector2d operator+(const Vector2d& right) { // why we remember function name? we can overload operator.
+        Vector2d result; // there is no default constructor. So, there is an error. It is default constructor.
+
+        result.a = this->a + right.a;
+        result.b = this->b + right.b;
+        return result;
+        // copy it to object inside.
+    }
 };
 
-//Vector2d addVectors(const Vector2d& first, const Vector2d& second) { // const keyword
-//    Vector2d result;
-//    first.a = first.b; // you cannot change attributes of const variable.
-//    result.a = first.a + second.a;
-//    result.b = first.b + second.b;
-//    return result;
-//    // copy it to object inside.
-//}
 
-int main(int argc, char* argv[]) {
-
-    Vector2d A(5,7); //when main function exits, A destroyed.
+void func() {
+    Vector2d A(5,7);
 
     Vector2d B(-1,3);
 
-    // add 2d vectors
-    Vector2d C = A.addVectors(B);
+    Vector2d C = A+B;
+}
+
+int main(int argc, char* argv[]) {
+    func();
+
 
     return 0;
 }
-
-// add vectoru mainden sonraki kısma yazarsan compiler hata verir. Eğer metodun signature ını mainden önce yazarsan
-// compiler sana güvenir. eğer bu signature de bir metod yoksa compiler link error verir.
