385d86b02a23fd279a7463d043bf1f203c49907d
muhsinertugruldogan <ertugrul.dogan@ozu.edu.tr>
2023-10-28 19:34:35 +0300

    improving OOP.

--- week3_app1.cpp
+++ week3_app1.cpp
@@ -36,25 +36,52 @@
 int i = 10; // global scope, it is static initialization. it is not in stack, not in heap.
 
 struct Vector2d {
-    int a;
+    int a;  // member variable. attribute is used in OOP.
     int b;
+    char* c;
+    char* d;
 
 
     Vector2d(int a, int b) {
-        // this is pointer.
-        // a = a_
-        // b = b_ is a solution.
+        c = new char[100];
+        d = new char[100];
         this -> a = a;
         this -> b = b;
     }
+
+    ~Vector2d() {
+        // destructor.
+        // deleting a variable and array is different.
+        // new kelimesini kullandıklarını sil. diğerlerine gerek yok.
+        // RAII principle. If you use it cleverly, garbage collector becomes insufficient.
+
+        delete d;
+        delete[] c;
+    }
+
+    Vector2d addVectors(const Vector2d& second) { // const keyword
+        Vector2d result;
+
+        result.a = this-> a + second.a;
+        result.b = this-> b + second.b;
+        return result;
+        // copy it to object inside.
+    }
+
+//    int a;  attributelar class ın başında olmak zorunda değil. compiler ilk önce attribute ları görür.
+//    int b;
+//    char* c;
+//    char* d;
 };
 
-Vector2d addVectors(Vector2d first, Vector2d second) {
-    Vector2d result;
-    result.a = first.a + second.a;
-    result.b = first.b + second.b;
-    return result;
-}
+//Vector2d addVectors(const Vector2d& first, const Vector2d& second) { // const keyword
+//    Vector2d result;
+//    first.a = first.b; // you cannot change attributes of const variable.
+//    result.a = first.a + second.a;
+//    result.b = first.b + second.b;
+//    return result;
+//    // copy it to object inside.
+//}
 
 int main(int argc, char* argv[]) {
 
@@ -63,7 +90,10 @@ int main(int argc, char* argv[]) {
     Vector2d B(-1,3);
 
     // add 2d vectors
-    Vector2d C = addVectors(A,B);
+    Vector2d C = A.addVectors(B);
 
     return 0;
 }
+
+// add vectoru mainden sonraki kısma yazarsan compiler hata verir. Eğer metodun signature ını mainden önce yazarsan
+// compiler sana güvenir. eğer bu signature de bir metod yoksa compiler link error verir.
