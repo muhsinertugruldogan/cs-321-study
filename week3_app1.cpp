1f29e083ea33bcdf762a01d28c5d4ee54a2d061a
muhsinertugruldogan <ertugrul.dogan@ozu.edu.tr>
2023-10-28 19:20:32 +0300

    birth of OOP of.

--- week3_app1.cpp
+++ week3_app1.cpp
@@ -35,41 +35,32 @@
 
 int i = 10; // global scope, it is static initialization. it is not in stack, not in heap.
 
-struct Vector2d {int a; int b; };
-// this func() is free.
-Vector2d addVectors(Vector2d first, Vector2d second) { //they are cloned. it is pass by value.
+struct Vector2d {
+    int a;
+    int b;
+
+
+    Vector2d(int a, int b) {
+        // this is pointer.
+        // a = a_
+        // b = b_ is a solution.
+        this -> a = a;
+        this -> b = b;
+    }
+};
+
+Vector2d addVectors(Vector2d first, Vector2d second) {
     Vector2d result;
     result.a = first.a + second.a;
     result.b = first.b + second.b;
     return result;
 }
 
-//void initVector2d(Vector2d* v, int a, int b) { //pointer cloned
-//    // pointers are very dangerous. we can change it with reference.
-//    // pointers are primitive type.
-//    v->a = a;
-//    v->b = b;
-//}
-void initVector2d(Vector2d& v, int a, int b) { //reference
-    // reference is higher level encapsulation of pointers.
-    // references do not allow pointer arithmetic.
-
-
-    v.a = a;
-    v.b = b;
-}
-
 int main(int argc, char* argv[]) {
-    // initialization of vector2d's are messy.
-    // first 2d vector instance.
-    Vector2d A;
-    initVector2d(A, 5, 7); // memory address cloned.
-    // A changed by pointer.
 
-    // second 2d vector instance
-    Vector2d B;
-    B.a = -1;
-    B.b = -3;
+    Vector2d A(5,7); //when main function exits, A destroyed.
+
+    Vector2d B(-1,3);
 
     // add 2d vectors
     Vector2d C = addVectors(A,B);
